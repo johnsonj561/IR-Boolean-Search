9ee0a939f5d36c46824fd31e982bc277d519baae
Sivia Franko <siviafranko@Sivias-MacBook-Air.local>
2017-08-28 20:25:50 -0400

    Refactored, built a collection of documents and tokenized.

--- main.js
+++ main.js
@@ -1,30 +1,18 @@
-const fse = require('fs-extra'),
-  CleanTextStream = require('./boolean_search_modules/TransformStreams/clean-text-stream'),
-  TimePrecision = require('./boolean_search_modules/TimePrecision/time-precision'),
-  DocumentDictionary = require('./boolean_search_modules/DocumentDictionary/document-dictionary');
+const TimePrecision = require('./boolean_search_modules/TimePrecision/time-precision');
+const TextDocument = require('./boolean_search_modules/TextDocument/text-document');
+const TextCollection = require('./boolean_search_modules/TextCollection/text-collection');
 
 console.log('\n\nMAIN\n\n');
 
-// eventually, collection will contain references to all Documents, including their DocID and term list
-let collection = {
-  1: {
-    id: 1,
-    path: 'colletion/document1.txt'
-  },
-  2: {
-    id: 2,
-    path: 'collection/document2.txt'
-  },
-  3: {
-    id: 3,
-    path: 'collection/document3.txt'
-  }
-};
+// Store paths to text documents
+const textFilePaths = [
+  'collection/document1.txt',
+  'collection/document2.txt',
+  'collection/document3.txt',
+];
 
-// testing with document 3 temporarily
-const docID = 3;
-collection[docID].document = new DocumentDictionary(docID);
-const filePath = 'collection/document3.txt';
+// define a TextCollection object
+let textCollection = new TextCollection();
 
 // define transformation options to perform on data
 const transformOptions = {
@@ -34,33 +22,28 @@ const transformOptions = {
   thisOptionShouldntWork: true
 }
 
-// create transform stream for cleaning text
-const cleanTextStream = new CleanTextStream(transformOptions);
+let msTimes = [];
 
-// start time
-const start = TimePrecision.getTimeMS();
-
-// read from file and pipe into transform stream
-const readAndCleanStream = fse.createReadStream(filePath, 'utf-8').pipe(cleanTextStream)
-  .on('data', chunk => {
-    console.log('\n\nSTREAM CHUNK AFTER TRANSFORM: \n', chunk)
-    // split the cleaned text by space, creating array of words
-    // sort words to simplify merging
-    sortAndTerm(chunk);
-  })
-  .on('end', function () {
-    this.emit('textCleaning:complete');
-  })
-  .on('error', err => console.log('\n\nSTREAM ERROR AFTER TRANSFORM:\n ', err));
-
-/*
- * Split text by word into array and sort array
- * We actually don't need to sort if using object (hash), but will leave as is for now
- */
-function sortAndTerm(text) {
-  text = text.split(/\s/).sort();
-  collection[docID].document.addTerms(text);
-  collection[docID].document.print();
-  const end = TimePrecision.getTimeMS();
-  console.log('\n\nReading text document, clearning, sorting, and counting terms took: ' + TimePrecision.getTimeDifference(start, end, 'ms'));
+for (let i = 0, l = textFilePaths.length; i < l; i++) {
+  let time1 = TimePrecision.getTimeMS();
+  let textDoc = new TextDocument(textFilePaths[i]);
+  textDoc.tokenizeAndCountDocument(transformOptions, () => {
+    textCollection.addDocument(textDoc);
+    let time2 = TimePrecision.getTimeMS();
+    console.log('Document with ID: + ' + textDoc.id + ' completed in \t' + TimePrecision.getTimeDifference(time1, time2, 'ms'));
+    if (i === (l - 1)) collectionDone();
+  });
 }
+
+
+
+function collectionDone() {
+  console.log('\n\nTextCollection created with size: ', textCollection.documentListSize());
+  let retrievedDoc = textCollection.getDocument(1);
+  console.log('retrievedDoc has attributes: ');
+  console.log('wordCount: ', retrievedDoc.wordCount);
+  console.log('isTokenized: ', retrievedDoc.isTokenized);
+}
+
+
+console.log('\nProof of async tokenization...');
